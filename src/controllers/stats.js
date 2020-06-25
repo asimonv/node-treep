e482198479bcb1c47447ba620358e4a68840625c
asimonv <aasimon@uc.cl>
2020-06-24 21:26:14 -0400

    added stats back

--- src/controllers/stats.js
+++ src/controllers/stats.js
@@ -1,40 +1,40 @@
 const db = require('../models');
 
+// const getStats = async () => [
+//   {
+//     statType: 1,
+//     title: 'Stats soon...',
+//   },
+// ];
+
 const getStats = async () => [
   {
-    statType: 1,
-    title: 'Stats soon...',
+    statType: 0,
+    image: 'https://assets-ouch.icons8.com/preview/781/61f24427-f2dd-4ef8-8b50-4881e1ca5c69.png',
+    title: 'Top teachers',
+    subtitle: 'Based on a certain factor.',
+    url: 'teachers',
+    id: 'top-teachers',
+    description:
+      "There's no much to say. Treep calculates which teacher is better based
+      on his/her/etc popularity, knowledge and disposition which results in a
+      certain factor. The higher, the better!\n\nHere are the results:",
+  },
+  {
+    image: 'https://assets-ouch.icons8.com/preview/695/d251cbc9-cf3f-496c-a115-d6e8b8909345.png',
+    title: 'Top courses',
+    subtitle: 'Based on popularity and difficulty and other attributes.',
+    url: 'courses',
+    id: 'top-courses',
+  },
+  {
+    image: 'https://assets-ouch.icons8.com/preview/283/fa86f902-1195-444e-8a85-b20d7c592d62.png',
+    title: 'Top theological courses',
+    subtitle: 'Choose the best 🙏🛐🌟',
+    url: 'teologicos',
+    id: 'top-teologicos',
   },
 ];
-
-/*
-{
-  statType: 0,
-  image: 'https://assets-ouch.icons8.com/preview/781/61f24427-f2dd-4ef8-8b50-4881e1ca5c69.png',
-  title: 'Top teachers',
-  subtitle: 'Based on a certain factor.',
-  url: 'teachers',
-  id: 'top-teachers',
-  description:
-    "There's no much to say. Treep calculates which teacher is better based
-    on his/her/etc popularity, knowledge and disposition which results in a
-    certain factor. The higher, the better!\n\nHere are the results:",
-},
-{
-  image: 'https://assets-ouch.icons8.com/preview/695/d251cbc9-cf3f-496c-a115-d6e8b8909345.png',
-  title: 'Top courses',
-  subtitle: 'Based on popularity and difficulty and other attributes.',
-  url: 'courses',
-  id: 'top-courses',
-},
-{
-  image: 'https://assets-ouch.icons8.com/preview/283/fa86f902-1195-444e-8a85-b20d7c592d62.png',
-  title: 'Top theological courses',
-  subtitle: 'Choose the best 🙏🛐🌟',
-  url: 'teologicos',
-  id: 'top-teologicos',
-},
-*/
 
 const getBestTeachers = async () => {
   const teachers = db.Teacher.findAll({

8934158a4aee980c3c1c774439c31d78c166236e
asimonv <aasimon@uc.cl>
2020-07-15 01:04:18 -0400

    removed log

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,7 +5,6 @@ const authService = require("../controllers/auth");
 
 router.put("/", async (req, res, next) => {
   try {
-    console.log(req.body);
     const authRes = await authService.authUser(req.body);
     res.json(authRes);
   } catch (e) {

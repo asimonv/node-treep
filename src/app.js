dd8d9c9b4a77ef1242ec9f935da3638ebdcc10f4
asimonv <aasimon@uc.cl>
2020-07-14 23:44:03 -0400

    at this point i dont know anymore

--- src/app.js
+++ src/app.js
@@ -29,6 +29,16 @@ app.options(
   })
 ); // include before other routes
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Routing middleware
 app.use("/api", routes);
 
